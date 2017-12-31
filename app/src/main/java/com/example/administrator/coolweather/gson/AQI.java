b66080bb1a462b3d8ee326f1818d4cf5cf14775e
lushimeidev <1151599129@qq.com>
2017-12-31 12:32:56 +0800

    加入显示天气的功能

---
+++ app/src/main/java/com/example/administrator/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.example.administrator.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/12/30 0030.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+
+    }
+}
